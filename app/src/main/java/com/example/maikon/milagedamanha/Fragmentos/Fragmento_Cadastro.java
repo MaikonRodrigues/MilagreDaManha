2c37795fc3589b784f37167ca4da259481559cd0
Maikon Rodrigues <maikorodrigues787@gmail.com>
2019-05-14 00:36:24 -0300

    commit

--- app/src/main/java/com/example/maikon/milagedamanha/Fragmentos/Fragmento_Cadastro.java
+++ app/src/main/java/com/example/maikon/milagedamanha/Fragmentos/Fragmento_Cadastro.java
@@ -2,13 +2,20 @@ package com.example.maikon.milagedamanha.Fragmentos;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -24,6 +31,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class Fragmento_Cadastro extends Fragment implements Response.ErrorListener, Response.Listener<JSONObject> {
     View v;
     EditText email, senha, confSenha, nome;
@@ -33,7 +43,10 @@ public class Fragmento_Cadastro extends Fragment implements Response.ErrorListen
     JsonObjectRequest jsonObjectReq;
     User usuarioLogado;
     String idLogado;
+    ImageView imgUser;
+    Bitmap bitmap = null;
 
+    private static final int COD_SELECIONA = 10;
 
 
     private static final String REGISTER_URL="http://www.ellego.com.br/webservice/MilagDaManha/regUser.php";
@@ -47,8 +60,17 @@ public class Fragmento_Cadastro extends Fragment implements Response.ErrorListen
         senha = (EditText)  v.findViewById(R.id.password);
         confSenha = (EditText) v.findViewById(R.id.confirmPassword);
         btnCadastrar = (Button) v.findViewById(R.id.btnCadastrar);
+        imgUser = (ImageView) v.findViewById(R.id.imgUser);
 
-
+        FloatingActionButton fab = v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, COD_SELECIONA);
+            }
+        });
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,12 +79,15 @@ public class Fragmento_Cadastro extends Fragment implements Response.ErrorListen
                         senha.getText().toString().isEmpty() || confSenha.getText().toString().isEmpty() ){
                     Toast.makeText(v.getContext(), "Todos os campos são obrigatorios", Toast.LENGTH_SHORT).show();
                 }else {
-                    if (senha.getText().toString().equals(confSenha.getText().toString())){
-                        cadastrar();
-                        //registerUser();
-                    }else{
-                        Toast.makeText(v.getContext(), "As senhas não são iguais", Toast.LENGTH_SHORT).show();
-                    }
+
+                        if (senha.getText().toString().equals(confSenha.getText().toString())){
+                            cadastrar();
+                            //registerUser();
+                        }else{
+                            Toast.makeText(v.getContext(), "As senhas não são iguais", Toast.LENGTH_SHORT).show();
+                        }
+                    
+
                 }
             }
         });
@@ -105,6 +130,38 @@ public class Fragmento_Cadastro extends Fragment implements Response.ErrorListen
         Intent it = new Intent(v.getContext(), MainActivity.class).putExtra("id", jsonObject.optString("idusers"));
         it.putExtra("nome", jsonObject.optString("nome") );
         startActivity(it);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case COD_SELECIONA:
+                Uri tabPost = data.getData();
+                imgUser.setImageURI(tabPost);
+
+                try {
+                    bitmap=MediaStore.Images.Media.getBitmap(v.getContext().getContentResolver(),tabPost);
+                    imgUser.setVisibility(View.VISIBLE);
+                    imgUser.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+
+        }
+    }
+
+    private String converterImgString(Bitmap bitmap) {
+
+        ByteArrayOutputStream array=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,array);
+        byte[] imagemByte=array.toByteArray();
+        String imagemString= Base64.encodeToString(imagemByte,Base64.DEFAULT);
+
+        return imagemString;
     }
 
 }
